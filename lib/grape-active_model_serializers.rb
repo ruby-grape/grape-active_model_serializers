639daa9e4fcc3c4291f8aa6fce7449b50b67c4c4
Jonathan Richard Henry Evans <jonathan.r.h.evans@gmail.com>
2013-09-09 12:41:24 +0100

    Moves EndpointExtension into grape-active_model_serializers folder as we are not directly monkeypatching

--- lib/grape-active_model_serializers.rb
+++ lib/grape-active_model_serializers.rb
@@ -1,5 +1,5 @@
 require 'active_model_serializers'
 require 'grape'
-require 'grape/endpoint_extension'
-require "grape-active_model_serializers/version"
-require "grape-active_model_serializers/formatter"
+require 'grape-active_model_serializers/endpoint_extension'
+require 'grape-active_model_serializers/formatter'
+require 'grape-active_model_serializers/version'
