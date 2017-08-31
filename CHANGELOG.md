e479e0e635f1abb9f61ef55e0f40ed992bb63553
xn <christian.trosclair@gmail.com>
2017-08-31 10:21:35 -0500

    Fix rubocop formatting errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 ### 1.5.2 (Next)
 
-* Your contribution here.
+* [#76](https://github.com/ruby-grape/grape-active_model_serializers/pull/76): Add custom error formatter - [@xn](https://github.com/xn).
 
 ### 1.5.1 (April 25, 2017)
 
--- lib/grape-active_model_serializers/error_formatter.rb
+++ lib/grape-active_model_serializers/error_formatter.rb
@@ -7,24 +7,37 @@ module Grape
         def call(message, backtrace, options = {}, env = nil)
           result = wrap_message(present(message, env))
 
-          if (options[:rescue_options] || {})[:backtrace] && backtrace && !backtrace.empty?
+          if (options[:rescue_options] || {})[:backtrace] &&
+             backtrace && !backtrace.empty?
             result = result.merge(backtrace: backtrace)
           end
 
           MultiJson.dump(result)
-
         end
 
         private
 
         def wrap_message(message)
-          if message.respond_to?(:errors) && message.errors.is_a?(ActiveModel::Errors)
-            ::ActiveModelSerializers::SerializableResource.new(message, {serializer: ActiveModel::Serializer::ErrorSerializer}).as_json
-          elsif message.is_a?(Exceptions::ValidationErrors) || message.is_a?(Hash)
+          if active_model?(message)
+            ::ActiveModelSerializers::SerializableResource.new(
+              message,
+              serializer: ActiveModel::Serializer::ErrorSerializer
+            ).as_json
+          elsif ok_to_pass_through?(message)
             message
           else
             { error: message }
           end
+        end
+
+        def active_model?(message)
+          message.respond_to?(:errors) &&
+            message.errors.is_a?(ActiveModel::Errors)
+        end
+
+        def ok_to_pass_through?(message)
+          message.is_a?(Exceptions::ValidationErrors) ||
+            message.is_a?(Hash)
         end
       end
     end
